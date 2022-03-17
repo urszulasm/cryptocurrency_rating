0c2590a29b19b341e5c2f2b7828805ea0778a491
urszulasm <samol.urszula@gmail.com>
2022-03-17 22:24:37 +0100

    initial commit

---
+++ data_extraction.py
@@ -0,0 +1,12 @@
+import yfinance as yf
+
+# function to get historical data for specified tickers
+def get_historical_data(ticker: str):
+
+    yf_object = yf.Ticker(ticker)
+
+    yf_historical = yf_object.history(period='max')
+
+    # save data to csv file
+    yf_historical.to_csv(f'{ticker}_historic_data.csv')
+
---
+++ run.py
@@ -0,0 +1,6 @@
+from data_extraction import get_historical_data
+
+#Step 1 - BTC-USD and S&P 500 import
+get_historical_data('BTC-USD')
+get_historical_data('SPX')
+
