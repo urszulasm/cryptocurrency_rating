121da5f7e7b40a1f10871d8509cd6da72b8b4ca6
urszulasm <samol.urszula@gmail.com>
2022-03-18 09:32:04 +0100

    add 3rd step - merging dfs

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from data_extraction import get_historical_data
 from data_processing import data_preprocessing
+from data_processing import merge_dfs
 
 # Step 1 - BTC-USD and S&P 500 import
 get_historical_data('BTC-USD')
@@ -8,3 +9,6 @@ get_historical_data('SPX')
 # Step 2 - data preprocessing
 btc = data_preprocessing('BTC-USD')
 sp_500 = data_preprocessing('SPX')
+
+# Step 3 - merging data frames
+df_combined = merge_dfs([btc, sp_500])
