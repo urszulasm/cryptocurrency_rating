ebf81234a0a324a05146bbc9298fa255ff49d698
urszulasm <samol.urszula@gmail.com>
2022-03-18 15:08:36 +0100

    add columns with cumulative daily returns

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,9 @@
 from typing import List
 
+import numpy as np
 import pandas as pd
 from pandas import DataFrame
+import re
 
 
 def data_preprocessing(ticker: str) -> DataFrame:
@@ -24,7 +26,7 @@ def data_preprocessing(ticker: str) -> DataFrame:
     yf_df.drop('Date', axis=1, inplace=True)
 
     # renaming Close column to add ticker name
-    yf_df.rename(columns={'Close': f'Close {ticker}'}, inplace=True)
+    yf_df.rename(columns={'Close': f'{ticker}'}, inplace=True)
 
     return yf_df
 
@@ -38,3 +40,35 @@ def merge_dfs(list_of_dfs: List[DataFrame]) -> DataFrame:
 
     # dropping NaN values
     return df_combined.dropna()
+
+
+def calculate_pct_change(df: DataFrame) -> DataFrame:
+
+    # get column names
+    column_names = df.columns
+
+    # create new cols with % change based on previous day and return df w/o 1st row (with NaN values)
+    for name in column_names:
+        df[f'{name}_return_daily'] = df[name].pct_change(1)  # 1 for ONE day look back
+    return df.iloc[1:, :]
+
+
+def calculate_cumulative_return(df: DataFrame) -> DataFrame:
+
+    # get col names which contain 'return_daily' in text
+    pct_change_cols = [name for name in df.columns if 'return_daily' in name]
+
+    # create new cols with calculated cumulative daily returns using cumprod func
+    for name in pct_change_cols:
+        df[f'{name}_1'] = (1 + df[name]).cumprod() - 1
+
+    # replace 'daily_1' in col name with 'cumulative'
+    df.columns = [re.sub('daily_1', 'cumulative', c) for c in df.columns]
+
+    return df
+
+
+
+
+
+
